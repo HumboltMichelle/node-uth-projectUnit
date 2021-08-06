89e43c4f17dffb77da44656b0ed5343bb585cde9
dwayneRichards <dwaynerichards@gmail.com>
2021-08-06 17:08:21 -0400

    IPCs made, buffer to big to stringify tbc

--- childProcess/change_vowel_child.js
+++ childProcess/change_vowel_child.js
@@ -1,19 +1,20 @@
-import * as fs from "fs";
+const fs = require("fs");
 
 process.on("message", (message) => {
   console.log(`ipc created for CP ${message.vowels}`);
   console.time(`fin reading ${message.vowels}`);
-  let largeTxt = fs.readFileSync("../data/large.txt");
+  let largeTxt = fs.readFileSync("../data/large.txt", {
+    encoding: "utf8",
+  });
+  //reading file into mem sync
   console.timeEnd(`fin reading ${message.vowels}`);
+  console.log(typeof largeTxt);
   largeTxt = largeTxt.replaceAll(message.vowels[0], message.vowels[1]);
 
   console.log(
     `finished swapping ${message.vowels[0]} for ${message.vowels[1]}`
   );
   const vowels = message.vowels;
-  process.send({
-    largeTxt,
-    vowels,
-  });
+  process.send(largeTxt, vowels);
   process.exit();
 });
--- childProcess/main.js
+++ childProcess/main.js
@@ -1,5 +1,5 @@
-import * as fs from "fs";
-import { fork } from "child_process";
+const fs = require("fs");
+const { fork } = require("child_process");
 const vowelArr = [
   ["a", "e"],
   ["e", "i"],
@@ -12,14 +12,16 @@ console.log("starting");
 vowelArr.forEach((vowels) => {
   const childProcess = fork(`./change_vowel_child.js`);
   console.log("file to fork exists?", fs.existsSync("./change_vowel_child.js"));
+
   console.log("childProcess connected", childProcess.connected);
+
   console.log("vowels sending", vowels);
 
   console.log(
     `can we send files to childProcess ${vowels}`,
     childProcess.send({ vowels })
   );
-  childProcess.send({ vowels });
+  childProcess.send({ v: "v" });
 
   childProcess.on("message", (message) => {
     console.log(`parentProcess IPC ${message.vowels}`);
